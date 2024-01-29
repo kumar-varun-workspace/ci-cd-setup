f90e7c03ffacce20dfdb517c2619fa3b803bf4a2
Kumar Varun <varunkumar2@deloitte.com>
2024-01-29 10:55:05 +0530

    added the code for reporting | Varun Kumar

--- src/main/java/packageName/basic.java
+++ src/main/java/packageName/basic.java
@@ -1,4 +1,8 @@
 package packageName;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,6 +14,21 @@ public class basic {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.google.com");
+        //Reporting code starts below
+        ExtentSparkReporter reporter;
+        ExtentReports extent;
+        ExtentTest test;
+        String path="Reports/index.html";           //Path of the report
+        reporter=new ExtentSparkReporter(path);
+        reporter.config().setReportName("Automation test results");         //Sets the name of the reports
+        reporter.config().setDocumentTitle("Spicejet-P7 Batch Results");    //Sets the title of report in browser tab
+        extent=new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Environment","UAT");                    //Sets some additional info
+        extent.setSystemInfo("TesterName","Varun Kumar");             //Sets some additional info
+        test = extent.createTest("Test Case 1");                      //Sets the name of the test
+        test.log(Status.PASS,"Test case pass");                      //Mark the test case as pass or fail
+        extent.flush();
     }
 
 }
